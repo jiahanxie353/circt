51f1a24fef9b73008ad91e75b89a248aadc8221c
jiahanxie353 <jx353@cornell.edu>
2024-05-29 10:05:54 -0400

    include json for storing external memory data

--- include/circt/Dialect/Calyx/CalyxLoweringUtils.h
+++ include/circt/Dialect/Calyx/CalyxLoweringUtils.h
@@ -16,6 +16,7 @@
 
 #include "circt/Dialect/Calyx/CalyxHelpers.h"
 #include "circt/Dialect/Calyx/CalyxOps.h"
+#include "circt/Dialect/Calyx/json.hpp"
 #include "circt/Dialect/Comb/CombOps.h"
 #include "circt/Support/LLVM.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
@@ -32,6 +33,8 @@
 
 namespace circt {
 namespace calyx {
+
+using json = nlohmann::json;
 
 void appendPortsForExternalMemref(PatternRewriter &rewriter, StringRef memName,
                                   Value memref, unsigned memoryID,
@@ -425,6 +428,10 @@ public:
     return builder.create<TLibraryOp>(loc, getUniqueName(name), resTypes);
   }
 
+  json &getExtMemData() { return extMemData; }
+
+  const json &getExtMemData() const { return extMemData; }
+
 private:
   /// The component which this lowering state is associated to.
   calyx::ComponentOp component;
@@ -461,6 +468,9 @@ private:
 
   /// A mapping between the callee and the instance.
   llvm::StringMap<calyx::InstanceOp> instanceMap;
+
+  /// A json file to store external global memory data
+  json extMemData;
 };
 
 /// An interface for conversion passes that lower Calyx programs. This handles
